c805aba8d31a0bca1a55d2b4c00a322941f08354
woonglife <woonglife62@gmail.com>
2023-03-16 22:20:21 +0900

    feat: add cora cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/woonglife62/woongkie-talkie/pkg/logger"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "",
+	Short: "",
+	Long:  ``,
+	// Run: func(cmd *cobra.Command, args []string) {
+	// 	log.Println("rootCmd test")
+	// },
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		logger.Logger.Fatal(err.Error())
+		os.Exit(1)
+	}
+}
