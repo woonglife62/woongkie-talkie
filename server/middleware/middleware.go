0d4c03f051802a390bfd26e208ed96aef1c45320
woonglife <woonglife62@gmail.com>
2023-03-16 23:00:07 +0900

    feat: add auth, render

---
+++ server/middleware/middleware.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"github.com/labstack/echo/v4"
+	echoMiddle "github.com/labstack/echo/v4/middleware"
+)
+
+func Middleware(e *echo.Echo) {
+	e.Use(echoMiddle.Logger())
+	e.Use(echoMiddle.Recover())
+
+	// auth
+	auth(e)
+
+	// render
+	render(e)
+}
